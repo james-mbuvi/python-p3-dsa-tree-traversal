89e201eb1ab006d353a8b63463a12836a3821bb2
[james-mbuvi] <[james.mbuvi@student.moringaschool.com]>
2024-03-15 18:18:25 +0300

    completed compulsory

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,15 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        return self._search_by_id(self.root, target_id)
+
+    def _search_by_id(self, node, target_id):
+        if node['id'] == target_id:
+            return node
+        for child in node['children']:
+            result = self._search_by_id(child, target_id)
+            if result:
+                return result
+        return None
